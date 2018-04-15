4fe06f17e408ba87de3a21c6f3ff29cc9f295893
Kxze <me@kaze.rip>
2018-04-15 12:18:12 -0300

    Add redirect to login button

--- src/client/Layout/LoginButton.tsx
+++ src/client/Layout/LoginButton.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { login } from '../Actions';
 import { Button } from 'semantic-ui-react';
+import { Link } from "react-router-dom";
 
 const LoginButton = ({ dispatch }: any) => (
-    <Button onClick={() => dispatch(login())}>Login</Button>
+    <Button onClick={() => window.location.href = "http://localhost:3000/api/auth/discord"}>Login</Button>
 );
 
 export default connect()(LoginButton);
